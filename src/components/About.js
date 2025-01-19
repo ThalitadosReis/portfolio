367c74a8ca6228cb819d3dcaa931411608fca643
Thalita dos Reis <dosreistha@gmail.com>
2025-01-19 15:17:29 +0100

    improved readability in about component

--- src/components/About.js
+++ src/components/About.js
@@ -5,28 +5,32 @@ export default function About() {
   return (
     <section className="section" id="about">
       <div className="container mx-auto">
-      <SectionHeader step="01" title="About me" />
+        {/* Section Header */}
+        <SectionHeader step="01" title="About me" />
 
         <div className="flex flex-col lg:flex-row items-center lg:items-start gap-10">
+          {/* Intro Text */}
           <div className="flex-1 text-center lg:text-left">
             <h1 className="uppercase text-[30px] lg:text-[45px] leading-[1] text-center lg:text-left lg:break-words lg:whitespace-pre-wrap">
-              I use my {"\n"}background in {"\n"}
-              <span className="font-newyork font-normal">Hospitality </span>to
-              build
-              <span className="font-newyork font-normal">customer-focused</span>
-              solutions.
+              I use my{"\n"}
+              background in{"\n"}
+              <span className="font-newyork">Hospitality</span> to build{"\n"}
+              <span className="font-newyork">customer-focused</span>{"\n"}
+              solutions
             </h1>
           </div>
+
+          {/* About Me Text */}
           <div className="flex-1 text-base text-justify">
             <p className="indent-8 mb-3">
-              Hi there! I'm Thalita, a <span className="underline">junior</span> full-stack developer based in Zürich with
-              a passion for learning and innovation. Beyond web development, I’m
-              deeply engaged with nature and adventure—whether I’m snowboarding
-              through the Swiss Alps in winter or enjoying lakeside sunshine in
-              summer. I’m also a culinary enthusiast, constantly experimenting
-              with international recipes, a creative practice that parallels my
-              approach to coding: blending curiosity with structure to achieve
-              something new.
+              Hi there! I'm Thalita, a <span className="underline">junior</span>{" "}
+              full-stack developer based in Zürich with a passion for learning
+              and innovation. Beyond web development, I’m deeply engaged with
+              nature and adventure—whether I’m snowboarding through the Swiss
+              Alps in winter or enjoying lakeside sunshine in summer. I’m also a
+              culinary enthusiast, constantly experimenting with international
+              recipes, a creative practice that parallels my approach to coding:
+              blending curiosity with structure to achieve something new.
             </p>
             <p className="indent-8 mb-3">
               Before transitioning into tech, I worked in the hospitality
