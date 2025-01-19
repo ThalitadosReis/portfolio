c5ae434e4eb9035768fa037e3d609ac3c3e2b807
Thalita dos Reis <dosreistha@gmail.com>
2025-01-19 15:28:25 +0100

    improved readability in banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,14 +7,23 @@ export default function Banner() {
       id="home"
     >
       <div className="container mx-auto mb-10">
-        <h1 className="text-[35px] md:text-[60px] lg:text-[100px] leading-[0.8] mb-10 font-newyork">
+        {/* Title */}
+        <h1 className="text-[35px] md:text-[60px] lg:text-[100px] leading-[0.8] mb-6 font-newyork">
           Full-Stack Developer
         </h1>
-        <p className="text-sm font-bold text-black/50 mb-10 break-words whitespace-pre-wrap">
+
+        {/* Subtitle */}
+        <p className="text-sm font-bold text-black/50 mb-8 break-words whitespace-pre-wrap">
           I'm a Full-Stack Developer based in Zürich.{"\n"}
           I specialize in React and JavaScript.
         </p>
-        <div className="bg-banner bg-cover bg-center bg-no-repeat w-full h-[300px] lg:h-[450px] max-w-[250px] lg:max-w-[450px] mx-auto grayscale"></div>
+
+        {/* Banner Image */}
+        <div
+          className="bg-banner bg-cover bg-center bg-no-repeat w-full h-[300px] lg:h-[450px] max-w-[250px] lg:max-w-[450px] mx-auto grayscale"
+          role="img"
+          aria-label="A picture of me with a soft gray background"
+        ></div>
       </div>
     </section>
   );
